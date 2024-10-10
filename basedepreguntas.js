97cd693d6dccebb9971869f684c1b2e2d55bda14
davidaliri <davidaliri95@gmail.com>
2024-10-09 19:29:07 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,145 @@
+basedepreguntas = [
+    {
+
+      pregunta: `Según el texto leído ¿Qué descubrimiento marca el paso de la Prehistoria a la Historia?`,
+      imagen: `https://i.ibb.co/hff2knq/tht.png`,
+      respuesta: `	La invención de la escritura.`,
+      distractores: [`	La aparición del hombre.`, `	La autonomía.`, `	Dependencia política`],
+      ayuda: `También pintó la última cena`,
+      ayudaImg: `https://i.ibb.co/wgZZvKc/obras-de-arte-leonardo-da-vinci-google-498711013-153997319-1024x576.jpg`,
+    },
+    {
+      imagen: `https://i.ibb.co/DGmyRWX/2.png` ,
+      pregunta: `Después de analizar las dos fuentes identifica a que especie de homo corresponde la información brindada.`,
+      respuesta: `	Homo Sapiens Sapiens.`,
+      distractores: [`	Homo Habilis.`, `Homo Erectus.`, `	Homo Neanderthalensis.`],
+      ayuda: `es lo mismo que 2*2`,
+      ayudaImg: `https://i.ibb.co/k1WgGW2/descarga.jpg`,
+    },
+    {
+      imagen: `https://i.ibb.co/zGgB2Zk/3.png` ,
+      pregunta: `Después del análisis, según la relación temporal con el pasado identifica a ¿Qué tipos de fuentes históricas corresponde?`,
+      respuesta: `Fuente escrita y fuente visual.`,
+      distractores: [`	Fuentes monumentales y fuentes escritas.`, `Fuentes secundaria y fuente primaria.`, `Manizalesd.	Fuente audiovisual y fuente primaria.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/j3FfFXH/4.png` ,
+      pregunta: `Habiendo realizado el análisis de las fuentes histórica, indica cuál de las siguientes alternativas corresponde al horizonte temprano.`,
+      respuesta: `Cultura Chavín`,
+      distractores: [`Imperio Wari`, `Imperio Inca`, `	Cultura Moche.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/pjn9D9J/5.png` ,
+      pregunta: `¿Cuál fue el proceso histórico que se desarrolló en primer lugar?`,
+      respuesta: `Horizonte temprano.`,
+      distractores: [`Horizonte tardío.`, `Intermedio temprano.`, `	Horizonte medio.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/T0F7f0g/6.png` ,
+      pregunta: `Habiedo comprendido el texto, deduce ¿Qué asentamiento humano se desarrollo en el valle del Cusco?`,
+      respuesta: `Killke.`,
+      distractores: [`Incas.`, `	Nazca.`, `	Tiahuanaco.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/VTFCrMd/7.png` ,
+      pregunta: `Basandonos en las fuentes, ¿Qué tipo de herramientas usaban nuestros antepasados al inicio de la evolución tecnológica del Paleolitico?`,
+      respuesta: `Piedras, huesos y palos`,
+      distractores: [`	Teléfonos y computadoras`, `	Martillos y clavos`, `	Lápices y papel`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/qDBfHf5/8.jpg` ,
+      pregunta: `¿Cuál de estas culturas o imperios eran agricultores aldeanos?`,
+      respuesta: `Culturas regionales y Formativo (influencia de la cultura Chavín)`,
+      distractores: [`	Imperio del Tahuantinsuyo, estados regionales e Imperio Huari`, `	Arcaico y Lítico`, `	Imperio Inca`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/QCVzTKk/11.jpg.png` ,
+      pregunta: `¿En qué imagen predomina los elementos naturales y en dónde los elementos sociales?`,
+      respuesta: `	Elementos naturales en el desierto y elementos sociales en las estribaciones andinas.`,
+      distractores: [`	Elementos naturales en la depresión y elementos sociales en las lomas.`, `	Elementos naturales en las estribaciones andinas y elementos sociales en las lomas.`, `Elementos naturales en el desierto y elementos sociales en la depresión.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/6v6FNPq/12-png.jpg` ,
+      pregunta: `¿Qué recursos naturales posee la costa, el mar peruano y que actividades económicas se realizan?`,
+      respuesta: `Recursos naturales como algodón, peces, gas natural y se realizan las actividades económicas de la agricultura y la pesca.`,
+      distractores: [`	Recursos naturales como el caucho y se realiza la actividad económica de la extracción del petróleo.`, `	Actividades económicas como ganadería porcina, extracción de arroz y recursos naturales como pesca y navegación.`, `	Recursos naturales como el petróleo y se realizan la actividad económica de la extracción del caucho.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/T2kFgDF/13.png` ,
+      pregunta: `Después de leer la fuente, responde ¿Por qué se mueren los camélidos andinos y que sucede con el agua de Alto Condoroma?`,
+      respuesta: `	El pueblo de Condoroma se fusiono con la mina y el agua contaminada no permitió la existencia de peces.`,
+      distractores: [`	Por la presencia de la mina y el pueblo se mudó, porque en esa zona no había agua.`, `	Por la contaminación y la falta de agua en el pueblo.`, `	Por la presencia de desechos mineros y el agua de Alto Condoroma está totalmente contaminada.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/tsVDMF2/14.jpg` ,
+      pregunta: `Después de observar e interpretar responde ¿Qué grupos de edad serán mayoritarios en el 2030 según la pirámide poblacional?`,
+      respuesta: `	En varones de 10 a 14 años y mujeres de 30 a 34 años.`,
+      distractores: [`	En varones y mujeres de 0 a 4 años.`, `	En mujeres de 50 a 54 años y varones de 70 a 74 años.`, `	En mujeres de 0 a 4 años y varones de 20 a 24 años.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/wcPygGZ/15.jpg` ,
+      pregunta: `¿Cuál es la ubicación de la ciudad de Londres?`,
+      respuesta: `		Latitud 50° y longitud 0°.`,
+      distractores: [`		Latitud 0° y hemisferio norte.`, `		Se encuentra en el trópico de cáncer.`, `		Longitud 0° y latitud 40°.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/vv4KByp/16.jpg` ,
+      pregunta: `Según el mapa ¿Qué regiones se ven afectadas con las heladas?`,
+      respuesta: `	Junín, Puno y Apurímac.`,
+      distractores: [`		Amazonas, Piura y Huancavelica.`, `		Lambayeque, Ucayali y Cusco.`, `		Cusco, puno y La Libertad.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/h15mRyt/17.png` ,
+      pregunta: `¿Qué consecuencias tendría la falta de los recursos naturales en la amazonia peruana?`,
+      respuesta: `		Falta de oxígeno, las erosiones, la temperatura del planeta se desequilibraría.`,
+      distractores: [`		El lavado de activos y la falsedad documental.`, `		La pérdida de biodiversidad y el crimen organizado.`, `		La abundancia de oxígeno, la absorción de dióxido de carbono y la seguridad alimentaria.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/3r02T4Q/18.jpg` ,
+      pregunta: `De las necesidades presentadas en la imagen, cuáles serían las dos más importantes para la subsistencia del ser humano en cualquier medio:`,
+      respuesta: `	Alimentación y salud.`,
+      distractores: [`			Seguridad y comunicación.`, `		Comunicación y ropa.`, `			Ocio y alimentación`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/WDctKh4/19.jpg` ,
+      pregunta: `Marca la alternativa correcta, según la clasificación de bienes y servicios.`,
+      respuesta: `			La gasolina y el coche son bienes complementarios.`,
+      distractores: [`		La lavadora es un tipo de bien libre.`, `		La harina es un servicio.`, `	El móvil es un bien público.	`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/dWmjRHL/20.png` ,
+      pregunta: `De acuerdo al texto leído ¿Cuál es el rol del Estado en el desarrollo de la ciencia y la tecnología en el país?`,
+      respuesta: `		Fomentar la investigación.`,
+      distractores: [`				La prevención de desastres.`, `		Otorgar becas para estudiantes.`, `			Aumentar PBI al sector educación.`],
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ];
